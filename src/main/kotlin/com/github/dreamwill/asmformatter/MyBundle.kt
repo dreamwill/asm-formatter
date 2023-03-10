ab845642b1aa34b5e6d7bc445dd968ec38b71c8c
GitHub Action <action@github.com>
2023-03-10 09:01:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/dreamwill/asmformatter/MyBundle.kt
+++ src/main/kotlin/com/github/dreamwill/asmformatter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dreamwill.asmformatter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dreamwill/asmformatter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dreamwill/asmformatter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dreamwill.asmformatter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dreamwill.asmformatter.MyBundle
 
 class MyApplicationService {
 
